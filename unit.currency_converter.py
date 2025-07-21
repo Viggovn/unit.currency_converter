b1e9d31061431abcb6e971d63f1cc9961391645a
Viggo van Nuil <viggovannuil@gmail.com>
2025-07-21 16:00:18 +0200

    first commit

--- unit.currency_converter.py
+++ unit.currency_converter.py
@@ -1,1 +1,27 @@
+from PySide6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QMainWindow
+from PySide6.QtCore import Qt
+import json
+import sys
+
+class Mainwindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.temp_layout = QHBoxLayout()
+        self.temp_button = QPushButton()
+        self.temp_button.clicked.connect(self.change_window_currency)
+        self.temp_layout.addWidget(self.temp_button)
+        self.setLayout(self.temp_layout)
+
+    def change_window_currency(self, checked):
+        window = currency_converter_window()
+        window.show()
+
+class currency_converter_window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+app = QApplication(sys.argv)
+window = Mainwindow()
+window.show()
+app.exec()
 
