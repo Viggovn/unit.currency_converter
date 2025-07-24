8e809a3dd54b9691a5d8ae34de642fb188a24a1f
Viggo van Nuil <viggovannuil@gmail.com>
2025-07-24 11:41:54 +0200

    basic layout

--- unit.currency_converter.py
+++ unit.currency_converter.py
@@ -1,27 +1,73 @@
-from PySide6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QMainWindow
+from PySide6.QtWidgets import (QApplication, QLabel, QWidget, 
+                               QVBoxLayout, QHBoxLayout, QPushButton, 
+                               QStackedLayout, QLineEdit, QComboBox)
 from PySide6.QtCore import Qt
 import json
 import sys
 
-class Mainwindow(QMainWindow):
+class UnitConverterWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.temp_layout = QHBoxLayout()
-        self.temp_button = QPushButton()
-        self.temp_button.clicked.connect(self.change_window_currency)
-        self.temp_layout.addWidget(self.temp_button)
-        self.setLayout(self.temp_layout)
+        self.MainLayout = QVBoxLayout()
 
-    def change_window_currency(self, checked):
-        window = currency_converter_window()
-        window.show()
 
-class currency_converter_window(QWidget):
+        self.ComboBoxLayout = QHBoxLayout()
+
+        self.select_unit_one = QComboBox()
+        self.select_unit_one.addItems(["mm", "cm", "dm", "m", "dam", "hm", "km"])
+        self.select_unit_two = QComboBox()
+        self.select_unit_two.addItems(["mm", "cm", "dm", "m", "dam", "hm", "km"])
+        self.ComboBoxLayout.addWidget(self.select_unit_one)
+        self.ComboBoxLayout.addWidget(self.select_unit_two)
+
+
+        self.input_Layout = QHBoxLayout()
+
+        self.input = QLineEdit("")
+        self.input_Layout.addWidget(self.input)
+        self.input_2 = QLineEdit("")
+        self.input_Layout.addWidget(self.input_2)
+
+        
+        self.Btn_Layout = QHBoxLayout()
+
+        self.Btn_convert = QPushButton()
+        self.Btn_convert.clicked.connect(self.temp_feature)   
+        self.Btn_Layout.addWidget(self.Btn_convert)  
+
+
+        self.MainLayout.addLayout(self.ComboBoxLayout)
+        self.MainLayout.addLayout(self.input_Layout)
+        self.MainLayout.addLayout(self.Btn_Layout)
+        self.setLayout(self.MainLayout)
+
+    def temp_feature(self):
+        if self.input.text():
+            print(self.input.text().strip())
+        elif self.input_2.text():
+            print(self.input.text().strip())
+        else:
+            print("no text")
+
+class CurrencyConverterWindow(QWidget):
     def __init__(self):
         super().__init__()
+
+class Mainwindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.multiple_windows = QStackedLayout(self)
+        self.unit_window = UnitConverterWindow()
+        self.currency_window = CurrencyConverterWindow()
+        self.multiple_windows.addWidget(self.unit_window)
+        self.multiple_windows.addWidget(self.currency_window)
+        self.multiple_windows.setCurrentIndex(0)
+
+        
 
 app = QApplication(sys.argv)
 window = Mainwindow()
+window.resize(800, 600)
 window.show()
-app.exec()
+sys.exit(app.exec())
 
